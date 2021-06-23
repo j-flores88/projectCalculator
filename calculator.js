e95aa97876fe0a7b33d85f172f6d2819b70cd6c5
Joel Flores <j_flores88@outlook.com>
2021-06-23 14:06:18 -0700

    Simplify code, remove unnecesary code

--- calculator.js
+++ calculator.js
@@ -1,132 +1,111 @@
+const buttons = document.querySelectorAll('button');
+const currentDisplay = document.querySelector('.current-operand');
+const prevDisplay = document.querySelector('.previous-operand');
+
 let currentOperand;
-let previousOperand;
+let prevOperand;
 let operator;
 
 
-
-const currentDisplay = document.querySelector('[data-current-operand]')
-const previousDisplay = document.querySelector('[data-previous-operand]')
-const buttons = document.querySelectorAll('button')
-
-const updateDisplay = () => {
-    currentDisplay.innerText = displayNumber(currentOperand);
-    if(operator !== undefined) {
-        previousDisplay.innerText = `${displayNumber(previousOperand)} ${operator}`
-    } else {
-        previousDisplay.innerText = displayNumber(previousOperand);
-    }
-
-}
-
 const clickBtn = () => {
+    clearDisplay()
     buttons.forEach(button => {
         button.addEventListener('click', () => {
-            if (button.classList.contains('operand')){
-                numberInput(button.innerText);             
-            } else if(button.classList.contains('operator')) {
+            if (button.classList.contains('operand')) {
+                numberInput(button.innerText);
+            } else if (button.classList.contains('operator')) {
                 operationInput(button.innerText)
                 operator = button.innerText
-            } else if(button.classList.contains('all-clear')) {
+            } else if (button.classList.contains('all-clear')) {
                 clearDisplay()
-            } else if(button.classList.contains('delete')){
+            } else if (button.classList.contains('delete')) {
                 deleteBtn()
-            } else if(button.classList.contains('equals')){
+            } else if (button.classList.contains('equals')) {
                 compute()
             }
         })
     })
 }
 
+
 window.addEventListener('keydown', (e) => {
     const key = document.querySelector(`button[data-key="${e.keyCode}"]`);
-        if(key.classList.contains('operand')) {
-            numberInput(key.innerText)
-        } else if(key.classList.contains('operator')) {
-            operationInput(key.innerText)
-            operator = key.innerText
-        } else if(key.classList.contains('all-clear')) {
-            clearDisplay()
-        } else if(key.classList.contains('delete')){
-            deleteBtn()
-        } else if(key.classList.contains('equals')){
-            compute()
-        }
+    if (key.classList.contains('operand')) {
+        numberInput(key.innerText)
+    } else if (key.classList.contains('operator')) {
+        operationInput(key.innerText)
+        operator = key.innerText
+    } else if (key.classList.contains('all-clear')) {
+        clearDisplay()
+    } else if (key.classList.contains('delete')) {
+        deleteBtn()
+    } else if (key.classList.contains('equals')) {
+        compute()
+    }
 })
 
-
 const numberInput = (operand) => {
-    if(operand === '.' && currentOperand.includes('.')) return;
-    currentOperand = currentOperand.toString() + operand.toString();
-    updateDisplay();
+    if (!operator) {
+        currentOperand += operand
+    } else if (operator && prevDisplay.innerHTML !== '') {
+        currentOperand += operand
+    }
+    currentDisplay.innerHTML = currentOperand;
 }
 
 const operationInput = (operator) => {
-    if(currentOperand === '') return;
-    if(previousOperand !== '') {
-        compute();
+    if (currentOperand === '') return;
+    if (prevOperand !== '') {
+        compute()
     }
-    previousOperand = currentOperand;
+    prevOperand = currentOperand;
     currentOperand = ''
+
+    prevDisplay.innerHTML = `${prevOperand} ${operator}`
+    currentDisplay.innerHTML = ''
     compute()
 }
 
 const compute = () => {
-    let computation;
-    const prev = parseFloat(previousOperand);
-    const current = parseFloat(currentOperand);
-    if(isNaN(prev) || isNaN(current)) return;
+    let answer;
+    let prev = Number(prevOperand);
+    let current = Number(currentOperand);
 
-    switch(operator) {
+    switch (operator) {
         case '+':
-            computation = prev + current;
+            answer = prev + current
             break;
         case '-':
-            computation = prev - current;
+            answer = prev - current
             break;
         case '*':
-            computation = prev * current;
+            answer = prev * current
             break;
         case '÷':
-            computation = prev / current;
+            answer = prev / current
             break;
         default:
             return
     }
-    currentOperand = computation;
-    operator = undefined;
-    previousOperand = '';
-    updateDisplay()
+    operator = ''
+    prevOperand = ''
+    currentOperand = answer;
+
+    prevDisplay.innerHTML = ''
+    currentDisplay.innerHTML = answer;
 }
 
-const displayNumber = (number) => {
-    const stringNumber = number.toString();
-    const integerDigits = parseFloat(stringNumber.split('.')[0]);
-    const decimalDigits = stringNumber.split('.')[1];
-    let integerDisplay;
-
-    if(isNaN(integerDigits)) {
-        integerDisplay = ''
-    } else {
-        integerDisplay = integerDigits.toLocaleString('en', {maximumFractionDigits: 0})
-    }
-    if(decimalDigits != null) {
-        return `${integerDisplay}.${decimalDigits}`
-    } else {
-        return integerDisplay
-    }
+const clearDisplay = () => {
+    currentOperand = '';
+    prevOperand = '';
+    operator = '';
+    currentDisplay.innerHTML = ''
+    prevDisplay.innerHTML = ''
 }
 
 const deleteBtn = () => {
     currentOperand = currentOperand.toString().slice(0, -1);
-    updateDisplay()
+    currentDisplay.innerHTML = currentOperand
 }
 
-const clearDisplay = () => {
-    currentOperand = ''
-    previousOperand = ''
-    operator = undefined;
-    updateDisplay()
-}
-
-clearDisplay();
 clickBtn()
